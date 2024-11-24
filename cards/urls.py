d2d9c9c8bb3ee5df4470f717205aa03fbced6931
KerlyLaeda <olgaperchenko1999@gmail.com>
2024-11-24 18:48:55 +0400

    First commit

---
+++ cards/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+#from . import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    # path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name="cards/index.html"), name="index")
+]
