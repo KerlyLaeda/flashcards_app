2d7e3ade731fbb40d768bba69b1e828b8f2a38c8
KerlyLaeda <olgaperchenko1999@gmail.com>
2024-11-26 15:23:37 +0400

    build: Add Card model, basic view, and basic templates

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NUM_BOXES = 5  # number of boxes for spaced repetition
+BOXES = range(1, NUM_BOXES + 1)  # user-friendly repr of boxes (1-5)
+
+
+class Card(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    box = models.IntegerField(
+        choices=zip(BOXES, BOXES),
+        default=BOXES[0],
+    )
+    date_created = models.DateTimeField(auto_now_add=True)  # to show newest first
+
+    def __str__(self):
+        return self.question
--- cards/urls.py
+++ cards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-#from . import views
-from django.views.generic import TemplateView
+from . import views
+# from django.views.generic import TemplateView
 
 
 urlpatterns = [
     # path("", views.index, name="index"),
-    path("", TemplateView.as_view(template_name="cards/index.html"), name="index")
+    # path("", TemplateView.as_view(template_name="cards/layout.html"), name="index"),
+    path("", views.CardListView.as_view(), name="card_list"),
 ]
